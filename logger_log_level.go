2832d069850c02c18ec29ddc22c4e17f2dc33f2f
sudzekai <sudzekai@hotmail.com>
2026-09-20 00:00:27 +0300

    LogLevel_NONE теперь последний в списке, что исправляет ошибку из-за которой все остальные уровни логирования писались, хотя стоял NONE

--- logger_log_level.go
+++ logger_log_level.go
@@ -3,10 +3,10 @@ package core
 type LogLevel int
 
 const (
-	LogLevel_NONE LogLevel = iota
-	LogLevel_DEBUG
+	LogLevel_DEBUG LogLevel = iota
 	LogLevel_INFORMATION
 	LogLevel_WARNING
 	LogLevel_ERROR
 	LogLevel_CRITICAL
+	LogLevel_NONE
 )
